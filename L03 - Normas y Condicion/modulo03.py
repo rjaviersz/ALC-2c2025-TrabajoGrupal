1977957cac9ccffc9ef626a94c27067b7ca76a0e
NehuenYago <nehuenromero@protonmail.com>
2025-09-14 19:14:13 -0300

    add norma

--- L03 - Normas y Condicion/modulo03.py
+++ L03 - Normas y Condicion/modulo03.py
@@ -1,7 +1,21 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Sep 14 14:54:04 2025
+import numpy as np
 
-@author: rjavi
-"""
-
+def norma(x,p):
+    """
+    Calcula la norma p del vector x sin np.linalg.norm
+    """
+    if p == 1:
+        for i in range(len(x)):
+            x[i] = abs(x[i])
+        return sum(x)
+    elif p == 2:
+        for i in range(len(x)):
+            x[i] = x[i]**2
+        return np.sqrt(sum(x))
+    elif p == np.inf:
+        for i in range(len(x)):
+            x[i] = abs(x[i])
+        return max(x)
+    else:
+        raise ValueError("p debe ser 1, 2 o np.inf")
+    
