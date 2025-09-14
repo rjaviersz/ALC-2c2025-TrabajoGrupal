dfad000a03d64f3a0f47a29eebec2c8e99cb38fa
NehuenYago <nehuenromero@protonmail.com>
2025-09-14 19:54:13 -0300

    add normaMatMC

--- L03 - Normas y Condicion/modulo03.py
+++ L03 - Normas y Condicion/modulo03.py
@@ -32,4 +32,20 @@ def normaMatMC(A,q,p,Np):
     """
     Devuelve la norma  ||A||{q , p} y el vector x en el cual se alcanza
     el maximo """
+    vectores_aleatorios = []
+    max_vector = None
+    max_norma = 0
     
+    for _ in range(Np):
+        vector_aleatorio = np.random.rand(A.shape[1])
+        vectores_aleatorios.append(vector_aleatorio)
+    vectores_aleatorios = normaliza(vectores_aleatorios, p)
+
+    for i in range(len(vectores_aleatorios)):
+        Ax = A @ vectores_aleatorios[i]
+        norma_Ax = norma(Ax, q)
+        if norma_Ax > max_norma:
+            max_norma = norma_Ax
+            max_vector = vectores_aleatorios[i]
+    return max_norma, max_vector
+
