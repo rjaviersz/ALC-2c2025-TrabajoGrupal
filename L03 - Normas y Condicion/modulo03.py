7bcf95d414e9a8e3500efd8323cd863f8e3ac008
rjaviersz <rjavier.suarez@hotmail.com>
2025-09-14 21:14:04 -0300

    update teso labo de Normas

--- L03 - Normas y Condicion/modulo03.py
+++ L03 - Normas y Condicion/modulo03.py
@@ -12,7 +12,7 @@ def norma(x,p):
         for i in range(len(x)):
             x[i] = x[i]**2
         return np.sqrt(sum(x))
-    elif p == np.inf:
+    elif p == "inf":
         for i in range(len(x)):
             x[i] = abs(x[i])
         return max(x)
@@ -49,3 +49,83 @@ def normaMatMC(A,q,p,Np):
             max_vector = vectores_aleatorios[i]
     return max_norma, max_vector
 
+def normaExacta(A, p=[1, 'inf']):
+    """
+    Devuelve una lista con las normas 1 e infinito de una matriz A
+    usando las expresiones del enunciado 2. (c).
+    """
+
+
+# Tests L03-Normas
+
+# Tests norma
+assert(np.allclose(norma(np.array([1,1]),2),np.sqrt(2)))
+assert(np.allclose(norma(np.array([1]*10),2),np.sqrt(10)))
+assert(norma(np.random.rand(10),2)<=np.sqrt(10))
+assert(norma(np.random.rand(10),2)>=0)
+
+# Tests normaliza
+# Tests normaliza
+for x in normaliza([np.array([1]*k) for k in range(1,11)],2):
+    assert(np.allclose(norma(x,2),1))
+for x in normaliza([np.array([1]*k) for k in range(2,11)],1):
+    assert(not np.allclose(norma(x,2),1) )
+for x in normaliza([np.random.rand(k) for k in range(1,11)],'inf'):
+    assert( np.allclose(norma(x,'inf'),1) )
+
+
+# Tests normaExacta
+
+# assert(np.allclose(normaExacta(np.array([[1,-1],[-1,-1]]),1),2))
+# assert(np.allclose(normaExacta(np.array([[1,-2],[-3,-4]]),1),6))
+# assert(np.allclose(normaExacta(np.array([[1,-2],[-3,-4]]),'inf'),7))
+# assert(normaExacta(np.array([[1,-2],[-3,-4]]),2) is None)
+# assert(normaExacta(np.random.random((10,10)),1)<=10)
+# assert(normaExacta(np.random.random((4,4)),'inf')<=4)
+
+# Test normaMC
+
+nMC = normaMatMC(A=np.eye(2),q=2,p=1,Np=100000)
+assert(np.allclose(nMC[0],1,atol=1e-3))
+assert(np.allclose(np.abs(nMC[1][0]),1,atol=1e-3) or np.allclose(np.abs(nMC[1][1]),1,atol=1e-3))
+assert(np.allclose(np.abs(nMC[1][0]),0,atol=1e-3) or np.allclose(np.abs(nMC[1][1]),0,atol=1e-3))
+
+nMC = normaMatMC(A=np.eye(2),q=2,p='inf',Np=100000)
+assert(np.allclose(nMC[0],np.sqrt(2),atol=1e-3))
+assert(np.allclose(np.abs(nMC[1][0]),1,atol=1e-3) and np.allclose(np.abs(nMC[1][1]),1,atol=1e-3))
+
+# A = np.array([[1,2],[3,4]])
+# nMC = normaMatMC(A=A,q='inf',p='inf',Np=1000000)
+# assert(np.allclose(nMC[0],normaExacta(A,'inf'),rtol=2e-1)) 
+
+# Test condMC
+
+# A = np.array([[1,1],[0,1]])
+# A_ = np.linalg.solve(A,np.eye(A.shape[0]))
+# normaA = normaMatMC(A,2,2,10000)
+# normaA_ = normaMatMC(A_,2,2,10000)
+# condA = condMC(A,2,10000)
+# assert(np.allclose(normaA[0]*normaA_[0],condA,atol=1e-3))
+
+# A = np.array([[3,2],[4,1]])
+# A_ = np.linalg.solve(A,np.eye(A.shape[0]))
+# normaA = normaMatMC(A,2,2,10000)
+# normaA_ = normaMatMC(A_,2,2,10000)
+# condA = condMC(A,2,10000)
+# assert(np.allclose(normaA[0]*normaA_[0],condA,atol=1e-3))
+
+# Test condExacta
+
+# A = np.random.rand(10,10)
+# A_ = np.linalg.solve(A,np.eye(A.shape[0]))
+# normaA = normaExacta(A,1)
+# normaA_ = normaExacta(A_,1)
+# condA = condExacta(A,1)
+# assert(np.allclose(normaA*normaA_,condA))
+
+# A = np.random.rand(10,10)
+# A_ = np.linalg.solve(A,np.eye(A.shape[0]))
+# normaA = normaExacta(A,'inf')
+# normaA_ = normaExacta(A_,'inf')
+# condA = condExacta(A,'inf')
+# assert(np.allclose(normaA*normaA_,condA))
