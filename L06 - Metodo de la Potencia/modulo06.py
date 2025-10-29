5a0495b854ea121ebf004ef3aecc5c81a3ee9b6c
rjaviersz <rjavier.suarez@hotmail.com>
2025-10-29 00:49:52 -0300

    modulo06 terminado

--- L06 - Metodo de la Potencia/modulo06.py
+++ L06 - Metodo de la Potencia/modulo06.py
@@ -175,6 +175,7 @@ def multiplicar_matrices(A,B):
     
     return AB
 
+
 #### TESTEOS
 # Tests metpot2k
 
@@ -243,123 +244,51 @@ for i in range(100):
 assert exitos >= 95
 
 
-#### TESTEOS
-# Tests diagRH
-
-def rand_matriz_simetrica(n):
-    A = np.random.random((n, n))
-    for i in range(n):
-        for j in range(n):
-            A[i,j] = A[j,i]
-    return A
-
-TAMANIO = 5
-# c) i
-exitos = 0
-for i in range(100):
-    A = rand_matriz_simetrica(TAMANIO) #podemos ir cambiando el tamaño
-    S,D = diagRH(A,tol=1e-15,K=1e5)
-    SDS_t = multiplicar_matrices(S, multiplicar_matrices(D, traspuesta(S)))
-    e = normaExacta(SDS_t - A, p='inf')
-    if e < 1e-5:
-        exitos += 1
-assert exitos >= 95
-    
-#c) ii
-suma = 0
-for i in range(100):
-    A = rand_matriz_simetrica(TAMANIO)
-    autovals = np.linalg.eigvalsh(A)
-    S,D = diagRH(A,tol=1e-15,K=1e5)
-    for i in range(TAMANIO):
-        suma += abs(D[i,i]) - abs(autovals[i])
-assert suma < 1e-15
-
-# d)
-N = 10
-tols = [1e-5, 1e-10, 1e-15]
-errores = []
-for t in tols:
-    for i in range(10):
-        A = rand_matriz_simetrica(N)
-        S,D = diagRH(A,tol=t,K=1e5)
-        norma_inf = normaExacta(multiplicar_matrices(S, multiplicar_matrices(D, traspuesta(S))) - A, p='inf')
-        errores.append((t, norma_inf))
-# print(errores)  # Descomentar para ver los errores obtenidos
-
-
-
 
 # =============================================================================
-#                         # Test L06-metpot2k, Aval
+#  Esto de abajo no va
 # =============================================================================
-
-
-#### TESTEOS
-# Tests metpot2k
-
-# S = np.vstack([
-#     np.array([2,1,0])/np.sqrt(5),
-#     np.array([-1,2,5])/np.sqrt(30),
-#     np.array([1,-2,1])/np.sqrt(6)
-#               ]).T
-
-# # Pedimos que pase el 95% de los casos
-# exitos = 0
-# for i in range(100):
-#     D = np.diag(np.random.random(3)+1)*100
-#     A = S@D@S.T
-#     v,l,_ = metpot2k(A,1e-15,1e5)
-#     if np.abs(l - np.max(D))< 1e-8:
-#         exitos += 1
-# assert exitos > 95
-
-
-# #Test con HH
-# exitos = 0
-# for i in range(100):
-#     v = np.random.rand(9)
-#     #v = np.abs(v)
-#     #v = (-1) * v
-#     ixv = np.argsort(-np.abs(v))
-#     D = np.diag(v[ixv])
-#     I = np.eye(9)
-#     H = I - 2*np.outer(v.T, v)/(np.linalg.norm(v)**2)   #matriz de HouseHolder
-
-#     A = H@D@H.T
-#     v,l,_ = metpot2k(A, 1e-15, 1e5)
-#     #max_eigen = abs(D[0][0])
-#     if abs(l - D[0,0]) < 1e-8:         
-#         exitos +=1
-# assert exitos > 95
-
-
-
+# #### TESTEOS
 # # Tests diagRH
-# D = np.diag([1,0.5,0.25])
-# S = np.vstack([
-#     np.array([1,-1,1])/np.sqrt(3),
-#     np.array([1,1,0])/np.sqrt(2),
-#     np.array([1,-1,-2])/np.sqrt(6)
-#               ]).T
 
-# A = S@D@S.T
-# SRH,DRH = diagRH(A,tol=1e-15,K=1e5)
-# assert np.allclose(D,DRH)
-# assert np.allclose(np.abs(S.T@SRH),np.eye(A.shape[0]),atol=1e-7)
+# def rand_matriz_simetrica(n):
+#     A = np.random.random((n, n))
+#     for i in range(n):
+#         for j in range(n):
+#             A[i,j] = A[j,i]
+#     return A
 
-
-
-# # Pedimos que pase el 95% de los casos
+# TAMANIO = 5
+# # c) i
 # exitos = 0
 # for i in range(100):
-#     A = np.random.random((5,5))
-#     A = 0.5*(A+A.T)
+#     A = rand_matriz_simetrica(TAMANIO) #podemos ir cambiando el tamaño
 #     S,D = diagRH(A,tol=1e-15,K=1e5)
-#     ARH = S@D@S.T
-#     e = normaExacta(ARH-A,p='inf')
-#     if e < 1e-5: 
+#     SDS_t = multiplicar_matrices(S, multiplicar_matrices(D, traspuesta(S)))
+#     e = normaExacta(SDS_t - A, p='inf')
+#     if e < 1e-5:
 #         exitos += 1
 # assert exitos >= 95
+    
+# #c) ii
+# suma = 0
+# for i in range(100):
+#     A = rand_matriz_simetrica(TAMANIO)
+#     autovals = np.linalg.eigvalsh(A)
+#     S,D = diagRH(A,tol=1e-15,K=1e5)
+#     for i in range(TAMANIO):
+#         suma += abs(D[i,i]) - abs(autovals[i])
+# assert suma < 1e-15
 
+# # d)
+# N = 10
+# tols = [1e-5, 1e-10, 1e-15]
+# errores = []
+# for t in tols:
+#     for i in range(10):
+#         A = rand_matriz_simetrica(N)
+#         S,D = diagRH(A,tol=t,K=1e5)
+#         norma_inf = normaExacta(multiplicar_matrices(S, multiplicar_matrices(D, traspuesta(S))) - A, p='inf')
+#         errores.append((t, norma_inf))
+# # print(errores)  # Descomentar para ver los errores obtenidos
 
